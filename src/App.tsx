f7e7c64ed32591490d4021526d923002bfcd4604
KarnesTH <p_haehnel@hotmail.de>
2025-07-15 17:53:02 +0200

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import DiagramView from "./components/DiagramView"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <>
       <div className="h-screen bg-bg text-primary">
-        <div className="flex flex-col items-center justify-center h-full">
+        <main className="flex flex-col items-center justify-center h-full">
           <DiagramView />
-        </div>
+        </main>
+        <Footer />
       </div>
     </>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className="w-full fixed bottom-6 h-10 bg-transparent text-text">
+			<div className="flex items-center justify-center">
+				<p className="text-lg">Created with ❤️ by <a target="_blank" title="KarnesTH's GitHub Profile" rel="noopener noreferrer" aria-label="KarnesTH's GitHub Profile" href="https://github.com/KarnesTH" className="text-primary hover:text-primary-hover">KarnesTH</a></p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
